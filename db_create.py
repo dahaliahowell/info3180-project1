9b3357c3afa90db9546f84600bb0252a6cf954e3
dahaliahowell <dahaliahowell.dh@gmail.com>
2021-03-23 02:25:15 -0500

    Edited config.py

--- db_create.py
+++ db_create.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from app import app # we import the app object from the app module
+from app import app
 from app import db
 
 db.create_all()
